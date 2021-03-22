d649cc94a0319617f10391032b3059b7407f662c
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-22 17:30:51 +0100

    add. snack 6 with es6

--- snack6/js/script.js
+++ snack6/js/script.js
@@ -1,29 +1,29 @@
 /*Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.*/
 
 //
-function generaArray (arrayUtente, a, b){
-
-  var newArray = [];
-
-  for (let i = 0; i < arrayUtente.length; i++){
-
-    if (i >= a && i <= b){
-
-      newArray.push(arrayUtente[i]);
-
-    }
-  }
-
-  return newArray;
-}
-
-var numeri = ["ciao", "cane", "pane", "ladro", "sedano"]
-
-
-
-var newArray = generaArray(numeri, 2, 4);
-
-console.log(newArray);
+// function generaArray (arrayUtente, a, b){
+//
+//   var newArray = [];
+//
+//   for (let i = 0; i < arrayUtente.length; i++){
+//
+//     if (i >= a && i <= b){
+//
+//       newArray.push(arrayUtente[i]);
+//
+//     }
+//   }
+//
+//   return newArray;
+// }
+//
+// var numeri = ["ciao", "cane", "pane", "ladro", "sedano"];
+//
+//
+//
+// var newArray = generaArray(numeri, 2, 4);
+//
+// console.log(newArray);
 
 // const students = {
 //   name: "paolo",
@@ -44,3 +44,21 @@ console.log(newArray);
 // // students.name = "pippo";
 // //
 // // console.log(students);
+
+
+
+
+const Numeri = ["ciao", "cane", "pane", "ladro", "sedano"];
+
+let min = parseInt(prompt("inserisci numero minimo"));
+
+let max = parseInt(prompt("inserisci numero massimo"));
+
+
+const newArray = Numeri.filter((item, index) => {
+
+  return (index >= min && index <= max)
+
+});
+
+console.log(newArray);
